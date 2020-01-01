091c853aa1ba5346fd1df66588bea96256b2a54a
Benardi <benardinunes@gmail.com>
2020-01-01 18:55:53 -0300

    Add stack documentation. Update overall docs

--- src/include/stack.h
+++ src/include/stack.h
@@ -19,10 +19,60 @@ typedef struct
     int top;
 } Stack;
 
+/** @brief Initializes Stack to have 0 elements.
+ *
+ *  Sets the attribute top to -1. Existing elements are
+ *  ignored.
+ *
+ *  @param pointer Stack to be initialized
+ *  @return Void
+ */
 void init_stack(Stack* stk);
+
+/** @brief Checks whether the stack is empty. 
+ *
+ *  If any element has been added to the stack 
+ *  returns true. If no element has been added 
+ *  or the stack has just been initialized returns
+ *  false. 
+ *
+ *  @param stk Stack as a pointer 
+ *  @return Whether stack is empty
+ */
 bool stack_empty(Stack* stk);
+
+/** @brief Checks whether the stack is full. 
+ *
+ *  If all available positions are occupied returns
+ *  true. If there's at least one available position 
+ *  or the stack has just been initialized returns
+ *  false. 
+ *
+ *  @param stk Stack as a pointer 
+ *  @return Whether stack is empty
+ */
 bool stack_full(Stack* stk);
+
+/** @brief Pushes element on top of stack. 
+ *
+ *  If there's an available position pushes 
+ *  element on top of stack and returns true.
+ *  Else does not change stack and returns false. 
+ *
+ *  @param stk Stack as a pointer 
+ *  @return Whether element could be pushed
+ */
 bool push(Stack* stk, Register reg);
+
+/** @brief Removes element from stack and returns it. 
+ *
+ *  If there's at least one element in stack removes 
+ *  and returns it as a pointer. Otherwise does not 
+ *  change stack and returns a @c NULL pointer. 
+ *
+ *  @param stk Stack as a pointer 
+ *  @return Whether an element could be popped
+ */
 Register* pop(Stack* stk);
 
 #endif /* STACK_H */
