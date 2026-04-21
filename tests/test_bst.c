337fd6a21014b0d1f5b4f504c881bfe4682eece9
Benardi <bnunes@ntst.com>
2026-04-21 20:09:51 -0300

    Remove bare usage of key with no local variable

--- tests/test_bst.c
+++ tests/test_bst.c
@@ -6048,7 +6048,6 @@ START_TEST(test_bst_iterative_tree_search_3)
   (*root) = NULL;
 
   k.i = 10;
-  key = k;
   reg->key = k;
   node1 = tree_insert(root, *reg, compare);
 
@@ -6064,7 +6063,7 @@ START_TEST(test_bst_iterative_tree_search_3)
   ck_assert_int_eq(node1->right == NULL, true);
   ck_assert_int_eq(node1->data.key.i, 10);
 
-  retrieved = iterative_tree_search(*root, key, compare);
+  retrieved = iterative_tree_search(*root, k, compare);
 
   ck_assert_int_eq(retrieved->p == NULL, true);
   ck_assert_int_eq(retrieved->left == NULL, true);
